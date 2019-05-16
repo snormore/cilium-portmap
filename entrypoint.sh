1cc3bbd76b665f593451cef29c091fdd769700d6
Steven Normore <snormore@digitalocean.com>
2019-05-16 08:06:08 -0400

    Remove unecessary cniVersion entry

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ while :; do
     if [ ! -f /host_etc_cni_netd/00-cilium-portmap.conflist ]; then
         cp /opt/cilium-portmap.conflist /host_etc_cni_netd/00-cilium-portmap.conflist
     fi
-    sleep 60
+    sleep 10
 done
